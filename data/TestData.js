3076c2e4b2b45331604f5be09f67cbf3976d84cf
Ayushkumar0508 <ayush.kumar@3pillarglobal.com>
2025-07-30 01:02:57 +0530

    Add TestData.js for central config, test users, app params.

---
+++ data/TestData.js
@@ -0,0 +1,12 @@
+// Centralized data for Saucedemo
+module.exports = {
+    username: 'standard_user',
+    password: 'secret_sauce',
+    firstName: 'John',
+    lastName: 'Doe',
+    postalCode: '12345',
+    timeouts: {
+        default: 50000,
+        interaction: 30000
+    }
+};
